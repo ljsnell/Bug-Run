7b3fe25b0da537407d98a8ab04f89722af072513
ljsnell <ljsnell7@gmail.com>
2020-04-04 20:35:42 -0500

    Up till here in the tutorial: https://pythonprogramming.net/pygame-drawing-shapes-objects/?completed=/adding-score-pygame-video-game/

--- bug-run.py
+++ bug-run.py
@@ -1,5 +1,6 @@
 import pygame
-
+import time
+import random
 
 pygame.init()
 
@@ -19,38 +20,96 @@ crashed = False
 bugImg = pygame.image.load('Bug1.png')
 bugImg = pygame.transform.scale(bugImg, (44, 44))
 
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
 def bug(x,y):
     gameDisplay.blit(bugImg, (x,y))
 
-x =  (display_width * 0.45)
-y = (display_height * 0.8)
-x_change = 0
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: "+str(count), True, black)
+    gameDisplay.blit(text,(0,0))
 
-gameExit = False
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
-while not gameExit:
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0        
-    x += x_change
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
 
-    gameDisplay.fill(white)
-    bug(x,y)
-
-    if x > display_width - bug_width or x < 0:
-        gameExit = True
-        
     pygame.display.update()
-    clock.tick(60)
 
+    time.sleep(2)
+
+    game_loop()
+
+def crash():
+    message_display('You Crashed')
+
+def game_loop():
+    x =  (display_width * 0.45)
+    y = (display_height * 0.8)
+    x_change = 0
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 100
+    thing_height = 100
+
+    thingCount = 1
+    dodged = 0
+
+    gameExit = False
+
+    while not gameExit:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0        
+        x += x_change
+
+        gameDisplay.fill(white)
+        # things(thingx, thingy, thingw, thingh, color)
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
+        bug(x,y)
+        things_dodged(dodged)
+
+        if x > display_width - bug_width or x < 0:
+            crash()
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
+
+        if y < thing_starty+thing_height:
+            print('y crossover')
+
+            if x > thing_startx and x < thing_startx + thing_width or x+bug_width > thing_startx and x + bug_width < thing_startx+thing_width:
+                print('x crossover')
+                crash()
+
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
 pygame.quit()
 quit()
