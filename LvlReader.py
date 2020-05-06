e5ba72f39ba61273a59c2d6931606d705946098d
ljsnell <ljsnell7@gmail.com>
2020-05-05 23:54:00 -0500

    Added logic to catch files which are too big.

--- LvlReader.py
+++ LvlReader.py
@@ -1,12 +1,21 @@
 import json
+import os
+import sys
 
 class LevelReader:
     """ Code to read in levels """
     
     def readInLevel(self, levelToRead):
-        with open('./levels/'+levelToRead) as json_file:
-            try:                
-                return json.load(json_file)
-            except ValueError as e:
-                print("Invalid Json for level: " + levelToRead)
-                quit()            
+        levelPath = './levels/'+levelToRead
+
+        # Ensure lvl is smaller than 1 GB
+        if os.stat(levelPath).st_size < 1073741824:
+            with open(levelPath) as json_file:
+                try:                
+                    return json.load(json_file)
+                except ValueError as e:
+                    print("Invalid Json for level: " + levelToRead)
+                    sys.exit()            
+        else:
+            print("Level file is too big!")
+            sys.exit()
