7e91300ab4977ccb0d4eb156f3fd4ce4cb5eab1b
ljsnell <ljsnell7@gmail.com>
2020-05-05 23:44:34 -0500

    Added error trapping for invalid json.

--- LvlReader.py
+++ LvlReader.py
@@ -4,5 +4,9 @@ class LevelReader:
     """ Code to read in levels """
     
     def readInLevel(self, levelToRead):
-        with open('./levels/'+levelToRead) as json_file:        
-            return json.load(json_file)
+        with open('./levels/'+levelToRead) as json_file:
+            try:                
+                return json.load(json_file)
+            except ValueError as e:
+                print("Invalid Json for level: " + levelToRead)
+                quit()            
